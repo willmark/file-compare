29696e991df4820eb949f707c25539216d2d8079
root <root@server.markjosephwilliams.com>
2013-12-11 17:29:35 +0000

    Added grunt support and code reduction for development

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    preprocess : {
+        js : {
+            src : 'index.js',
+            dest : 'src-pp/index.js'
+        }
+    },
+    jshint : {
+      'files': ['*.js']
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src-pp/**.js',
+        dest: 'dist/file-compare.min.js'
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  //verify  script
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Preprocessing to strip out development code
+  grunt.loadNpmTasks('grunt-preprocess');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'preprocess', 'uglify']);
+
+};
